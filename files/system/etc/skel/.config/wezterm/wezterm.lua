4f4fcc4afd58dc4cdbb887cb05a7fd97a947bfc0
CaderIdris <61825162+CaderIdris@users.noreply.github.com>
2024-11-17 17:20:33 +0000

    Updated wezterm config in skel to use separate distrobox plugin now

--- files/system/etc/skel/.config/wezterm/wezterm.lua
+++ files/system/etc/skel/.config/wezterm/wezterm.lua
@@ -1,37 +1,7 @@
 local wezterm = require 'wezterm'
-local act = wezterm.action
 local config = wezterm.config_builder()
 
-local function get_distrobox_containers ()
-	local flatpak_command = "flatpak-spawn --host distrobox list --no-color | awk -v col=3 '{print $col}' - | tail -n +2"
-	local wt_is_flatpak = os.execute(flatpak_command)
-	local on_host_command = "distrobox list --no-color | awk -v col=3 '{print $col}' - | tail -n +2"
-	local wt_is_host = os.execute(on_host)
-	local cmd = ""
-	if wt_is_flatpak then
-		cmd = flatpak_command
-	elseif wt_is_host then
-		cmd = on_host_command
-	else
-		wezterm.log_info("Distrobox not found")
-		return {}
-	end
-	local handle = io.popen(cmd)
-	local output = handle:read('*a')
-	local dbox_options = {}
-	for s in output:gmatch("[^\r\n]+") do
-		wezterm.log_info(s)
-		table.insert(
-			dbox_options,
-			{
-				label = tostring(s),
-				id = "distrobox - " .. tostring(s)
-			}
-		)
-	end
-	return dbox_options
-
-end
+local distrobox = wezterm.plugin.require("https://github.com/CaderIdris/distrobox.wezterm")
 
 config.window_close_confirmation = 'NeverPrompt'
 
@@ -69,63 +39,19 @@ config.keys = {
       mods = 'CTRL|ALT',
       action = wezterm.action.EmitEvent 'toggle-opacity',
     },
-	{
-    		key = 'R',
-		mods = 'CTRL|SHIFT',
-		action = wezterm.action_callback(function(window, pane)
+}
+-- Taken from https://www.lua.org/pil/13.4.3.html
+function set_default(t, d)
+	local mt = {__index = function () return d end}
+	setmetatable(t, mt)
+end
 
-
-			local choices = {
-				{
-					id = "",
-					label = wezterm.format {
-						{ Foreground = { AnsiColor = 'Teal' } },
-						{ Text = 'Host Shell' }
-					},
-				}
-			}
-			for _, v in ipairs(get_distrobox_containers()) do
-				table.insert(choices, v)
-			end
-
-			window:perform_action(
-				act.InputSelector {
-					action = wezterm.action_callback(
-						function(window, pane, id, label)
-							if not id and not label then
-								wezterm.log_info 'cancelled'
-							else
-								local cmd = {}
-								if string.find(id, "distrobox - ") then
-									cmd = {'distrobox', 'enter', '--name', tostring(label), '--no-workdir', '--', 'tmux'}
-								end
-								local mux_win = window:mux_window()
-								if id ~= "" then
-									local tab = mux_win:spawn_tab(
-										{
-											args = cmd,
-										}
-									)
-									wezterm.log_info('you selected ', label)
-								else
-									local tab = mux_win:spawn_tab(
-										{
-										}
-									)
-									wezterm.log_info('you selected host shell')
-								end
-							end
-						end
-						),
-						title = 'Containers',
-						choices = choices,
-						description = 'Choose your container',
-					},
-				pane
-			)
-			end
-		),
-	},
+local distrobox_enter_commands = {
+	["datblygiad"]="tmux",
+	["datblygiad-nvidia"]="tmux"
 }
 
+set_default(distrobox_enter_commands, "tmux")
+
+distrobox.apply_to_config(config, {entry_commands=distrobox_enter_commands})
 return config
